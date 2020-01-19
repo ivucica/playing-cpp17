8cb6de240ce021f1dbf68d7f846550403a993546
Ivan Vučica <ivan@vucica.net>
2020-01-19 20:25:42 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+playing-cpp17: playing-cpp17.o
+	g++ playing-cpp17.o -o playing-cpp17
+
---
+++ playing-cpp17.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (auto &v: {1, 2, 3, 4, 5, 6}) { cout << v; }
+}
